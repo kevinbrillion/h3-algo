2297cdc3987aaf22c41c85cba92fe50331527393
ChaseMeImFamous <kevin.brillion@gmail.com>
2017-10-23 03:38:20 +0200

    :tada: finished exercise 1 japanese

--- Exercise 1 - Japanese/success.php
+++ Exercise 1 - Japanese/success.php
@@ -1,4 +1,6 @@
 <?php
+// Consignes : afficher les caractères japonais un par un en renvenant à la ligne
+
 $contenuDuFichier = file_get_contents('texte-hiragana.txt');
 var_dump($contenuDuFichier);
 $pattern = "/[\x{3041}-\x{3096}\x{30A0}-\x{30FF}\x{3400}-\x{4DB5}\x{4E00}-\x{9FCB}\x{F900}-\x{FA6A}]/u";
